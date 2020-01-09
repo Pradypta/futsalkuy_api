a197cdc3acd7fdddbceaa3c67946f831ad94794c
Pradypta <dyptapra14@gmail.com>
2020-01-10 01:14:32 +0700

    editing v.3

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $api_url = $is_production ?
  'https://app.sandbox.midtrans.com/snap/v1/transactions';
 
 if( !strpos($_SERVER['REQUEST_URI'], '/charge')){
-    http_response_code(404)
+    http_response_code(404);
     echo "wrong path, make sure it's '/charge'"; exit();
 }
 if($_SERVER['REQUEST_METHOD'] !== 'POST'){
